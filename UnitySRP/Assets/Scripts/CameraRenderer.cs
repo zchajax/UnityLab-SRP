65d1715a7f1fe50cf97ea4332e1505846300f5d6
zchajax <zchajax@gmail.com>
2019-11-12 21:42:08 +0800

    Render skybox

---
+++ UnitySRP/Assets/Scripts/CameraRenderer.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEngine.Rendering;
+
+public class CameraRenderer
+{
+
+    ScriptableRenderContext context;
+
+    Camera camera;
+
+    public void Render(ScriptableRenderContext context, Camera camera)
+    {
+        this.context = context;
+        this.camera = camera;
+
+        Setup();
+        DrawVisiableGeometry();
+        Submit();
+    }
+
+    private void DrawVisiableGeometry()
+    {
+        context.DrawSkybox(camera);
+    }
+
+    private void Setup()
+    {
+        context.SetupCameraProperties(camera);
+    }
+
+    private void Submit()
+    {
+        context.Submit();
+    }
+}
---
+++ UnitySRP/Assets/Scripts/CustomRPAsset.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.Rendering;
+
+[CreateAssetMenu(menuName = "Rendering/Custom Render Pipeline")]
+public class CustomRPAsset : RenderPipelineAsset
+{
+    protected override RenderPipeline CreatePipeline()
+    {
+        return new CustomRP();
+    }
+}
+
+public class CustomRP : RenderPipeline
+{
+    CameraRenderer renderer = new CameraRenderer();
+    protected override void Render(ScriptableRenderContext context, Camera[] cameras)
+    {
+        foreach (var camera in cameras)
+        {
+            renderer.Render(context, camera);
+        }
+    }
+}
