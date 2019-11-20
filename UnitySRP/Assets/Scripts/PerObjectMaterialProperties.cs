ce40d058577cc76cd007a15052f195daff85e45c
zchajax <zchajax@gmail.com>
2019-11-20 22:48:30 +0800

    support many colors

---
+++ UnitySRP/Assets/Scripts/PerObjectMaterialProperties.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+[DisallowMultipleComponent]
+public class PerObjectMaterialProperties : MonoBehaviour
+{
+
+    static int baseColorId = Shader.PropertyToID("_BaseColor");
+
+    static MaterialPropertyBlock block;
+
+    [SerializeField]
+    Color baseColor = Color.white;
+
+    private void OnValidate()
+    {
+        if (block == null)
+        {
+            block = new MaterialPropertyBlock();
+        }
+
+        block.SetColor(baseColorId, baseColor);
+        GetComponent<Renderer>().SetPropertyBlock(block);
+    }
+}
